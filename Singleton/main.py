aeffc8d77ca0f69c4e1303a30a02a19a8977425d
Lynxtail <l.gorbachev@mail.ru>
2021-10-19 00:21:33 +0400

    add ex singleton

---
+++ Singleton/main.py
@@ -0,0 +1,25 @@
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class SingletonClass(metaclass=Singleton):
+    def function():
+        pass
+
+
+if __name__ == "__main__":
+    # Клиентский код.
+
+    s1 = Singleton()
+    s2 = Singleton()
+
+    if id(s1) == id(s2):
+        print("Singleton works, both variables contain the same instance.")
+    else:
+        print("Singleton failed, variables contain different instances.")
