5668a297e6c5b8fcfb0cf20ef550f1b1b440256d
Lynxtail <l.gorbachev@mail.ru>
2021-11-02 11:21:37 +0400

    edit Strategy

--- Strategy/main.py
+++ Strategy/main.py
@@ -1,94 +1,93 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import List
-
-
+import random
 class Context():
-    """
-    Контекст определяет интерфейс, представляющий интерес для клиентов.
-    """
 
-    def __init__(self, strategy: Strategy) -> None:
-        """
-        Обычно Контекст принимает стратегию через конструктор, а также
-        предоставляет сеттер для её изменения во время выполнения.
-        """
-
+    def __init__(self, strategy: Strategy):
         self._strategy = strategy
 
     @property
-    def strategy(self) -> Strategy:
-        """
-        Контекст хранит ссылку на один из объектов Стратегии. Контекст не знает
-        конкретного класса стратегии. Он должен работать со всеми стратегиями
-        через интерфейс Стратегии.
-        """
-
+    def strategy(self):
         return self._strategy
 
     @strategy.setter
-    def strategy(self, strategy: Strategy) -> None:
-        """
-        Обычно Контекст позволяет заменить объект Стратегии во время выполнения.
-        """
-
+    def strategy(self, strategy: Strategy):
         self._strategy = strategy
 
-    def do_some_business_logic(self) -> None:
-        """
-        Вместо того, чтобы самостоятельно реализовывать множественные версии
-        алгоритма, Контекст делегирует некоторую работу объекту Стратегии.
-        """
-
-        # ...
-
-        print("Context: Sorting data using the strategy (not sure how it'll do it)")
-        result = self._strategy.do_algorithm(["a", "b", "c", "d", "e"])
-        print(",".join(result))
-
-        # ...
+    def check_cup(self):
+        print(f"Checking cup {self._strategy.name}:")
+        print(self._strategy.do_algorithm())
 
 
 class Strategy(ABC):
-    """
-    Интерфейс Стратегии объявляет операции, общие для всех поддерживаемых версий
-    некоторого алгоритма.
-
-    Контекст использует этот интерфейс для вызова алгоритма, определённого
-    Конкретными Стратегиями.
-    """
-
     @abstractmethod
-    def do_algorithm(self, data: List):
+    def do_algorithm(self):
         pass
+    
 
+class CupA(Strategy):
+    name = 'Cup 1'
+    ball_there = False
 
-"""
-Конкретные Стратегии реализуют алгоритм, следуя базовому интерфейсу Стратегии.
-Этот интерфейс делает их взаимозаменяемыми в Контексте.
-"""
+    @property
+    def ball(self):
+        return self.ball_there
 
+    @ball.setter
+    def ball(self, ball):
+        self.ball_there = ball
 
-class ConcreteStrategyA(Strategy):
-    def do_algorithm(self, data: List) -> List:
-        return sorted(data)
+    def do_algorithm(self):
+        if self.ball == True:
+            return f"The ball is here!"
+        else:
+            return f"The ball isn't here!"
 
+class CupB(Strategy):
+    name = 'Cup 2'
+    ball_there = False
 
-class ConcreteStrategyB(Strategy):
-    def do_algorithm(self, data: List) -> List:
-        return reversed(sorted(data))
+    @property
+    def ball(self):
+        return self.ball_there
+
+    @ball.setter
+    def ball(self, ball):
+        self.ball_there = ball
+
+    def do_algorithm(self):
+        if self.ball == True:
+            return f"The ball is here!"
+        else:
+            return f"The ball isn't here!"
+
+class CupC(Strategy):
+    name = 'Cup 3'
+    ball_there = False
+
+    @property
+    def ball(self):
+        return self.ball_there
+
+    @ball.setter
+    def ball(self, ball):
+        self.ball_there = ball
+
+    def do_algorithm(self):
+        if self.ball == True:
+            return f"The ball is here!"
+        else:
+            return f"The ball isn't here!"
 
 
 if __name__ == "__main__":
-    # Клиентский код выбирает конкретную стратегию и передаёт её в контекст.
-    # Клиент должен знать о различиях между стратегиями, чтобы сделать
-    # правильный выбор.
+    cups = [CupA(), CupB(), CupC()]
+    random.choice(cups).ball = True
 
-    context = Context(ConcreteStrategyA())
-    print("Client: Strategy is set to normal sorting.")
-    context.do_some_business_logic()
+    context = Context(cups[0])
+    print(f"Player choose {context._strategy.name}:")
+    context.check_cup()
     print()
-
-    print("Client: Strategy is set to reverse sorting.")
-    context.strategy = ConcreteStrategyB()
-    context.do_some_business_logic()
+    context.strategy = cups[2]
+    print(f"Player choose {context._strategy.name}:")
+    context.check_cup()
